8932c89207f46933ecfd83736f67ccc6fc288173
wsoeltz <skiis652@gmail.com>
2014-03-03 20:04:39 -0500

    Updated student hub

--- js/tabs.js
+++ js/tabs.js
@@ -56,37 +56,37 @@ function hint(active){
     switch(active){
         case -1:
         	// Replaces with problem hint
-            var problem = '<h3>Need Help Writing your<h3 class="bold" style="margin-left: 4px">Problem?</h3><h3>Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah.</h3>';
+            var problem = '<h3>Need Help Writing your<h3 class="bold"=">&nbsp;Problem?</h3><p>What questions are you trying to answer? Include any preliminary observations or background information about the subject.</p>';
             $( "div#hintContainer" ).empty();
             $( "div#hintContainer" ).append(problem);
             break;
         case 0:
         	// Replaces with hypothesis hint
-            var hypothesis = '<h3>Need Help Writing your<h3 class="bold" style="margin-left: 4px">Hypothesis?</h3><h3>A hypothesis is an educated guess about how things work. Most of the time a hypothesis is written like this: "If _____[I do this]_____, then _____[this]_____ will happen." (Fill in the blanks with the appropriate information from your own experiment.)</h3>';
+            var hypothesis = '<h3>Need Help Writing your<h3 class="bold">&nbsp;Hypothesis?</h3><p>A hypothesis is an educated guess about how things work. Most of the time a hypothesis is written like this: "If _____[I do this]_____, then _____[this]_____ will happen." (Fill in the blanks with the appropriate information from your own experiment.)</p>';
             $( "div#hintContainer" ).empty();
             $( "div#hintContainer" ).append(hypothesis);
             break;
         case 1:
         	// Replaces with materials hint
-            var materials = '<h3>Need Help Writing your<h3 class="bold" style="margin-left: 4px">Materials?</h3><h3>Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah.</h3>';                    
+            var materials = '<h3>Need Help Writing your<h3 class="bold">&nbsp;Materials?</h3><p>Make a list of all items used in the lab, and in what quantity.</p>';                    
             $( "div#hintContainer" ).empty();
             $( "div#hintContainer" ).append(materials);
             break;
         case 2:
         	// Replaces with procedure hint
-            var procedure = '<h3>Need Help Writing your<h3 class="bold" style="margin-left: 4px">Procedure?</h3><h3>Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah.</h3>';
+            var procedure = '<h3>Need Help Writing your<h3 class="bold">&nbsp;Procedure?</h3><p>Use starting words such as obtain instead of get. Do not start each one with First, I went and got…etc. Remember another person may have to duplicate your lab, so it must be written in a clear, concise matter that will not cause error in their lab. Reproducibility is important for your experiment findings to be valid.</p>';
             $( "div#hintContainer" ).empty();
             $( "div#hintContainer" ).append(procedure);
             break;
         case 3:
         	// Replaces with result hint
-            var result = '<h3>Need Help Writing your<h3 class="bold" style="margin-left: 4px">Result?</h3><h3>Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah.</h3>';
+            var result = '<h3>Need Help Writing your<h3 class="bold">&nbsp;Result?</h3><p>Look over your data, determine if your experiment supports or refutes your hypothesis. Do not be afraid of being proven wrong. The experiment is just as valid as one in which you are proven correct. Include a summary of the data - averages, highest, lowest..etc to help the reader understand your results.</p>';
             $( "div#hintContainer" ).empty();
             $( "div#hintContainer" ).append(result);
             break;
         case 4:
         	// Replaces with conclusion hint
-            var conclusion = '<h3>Need Help Writing your<h3 class="bold" style="margin-left: 4px">Conclusion?</h3><h3>Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah.</h3>';
+            var conclusion = '<h3>Need Help Writing your<h3 class="bold">&nbsp;Conclusion?</h3><p>Explain why you accepted or rejected your hypothesis using data from the lab.Try not to copy your data here, you should summarize and reference key information. List one thing you learned and describe how it applies to a real-life situation. Discuss possible errors that could have occurred in the collection of the data.</p>';
             $( "div#hintContainer" ).empty();
             $( "div#hintContainer" ).append(conclusion);
             break;
