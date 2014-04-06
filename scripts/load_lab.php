729fcb56f7af9a07bb477d251a78a966e5afa562
Kaitlyn Carcia <kaitlyn_carcia@student.uml.edu>
2014-04-06 11:30:06 -0400

    Students can access the labs they've been working on.

---
+++ scripts/load_lab.php
@@ -0,0 +1,33 @@
+<?php
+	# Kaitlyn Carcia and Will Soeltz
+	# University of Massachusetts Lowell, 91.462 GUI Programming II, Jesse M. Heines
+	# File: load_lab.php
+	# Loads student previous work
+	# Last updated April 4, 2014 by KC
+
+	# connects to database
+	include "connect.php";
+
+	$id = $_GET['id'];
+	
+	# Selects all assignments given a specific Teacher ID
+	$result = mysql_query("SELECT * FROM Labs WHERE Lab_ID='$id'");
+	if (!$result) {
+		die('Invalid query: ' . mysql_error());
+	}
+	
+	# Gets any results from query
+	$values = mysql_fetch_array($result);
+
+	# Fill all the textareas with what's currently saved in the database
+	echo "<script type=\"text/javascript\">";
+		echo "$(function () {";
+			echo "$('#tabs-1-box').val('" . $values['Problem'] . "');";
+			echo "$('#tabs-2-box').val('" . $values['Hypothesis'] . "');";
+			echo "$('#tabs-3-box').val('" . $values['Materials'] . "');";
+ 			echo "$('#tabs-4-box').val('" . $values['Proced'] . "');";
+ 			echo "$('#tabs-5-box').val('" . $values['Results'] . "');";
+ 			echo "$('#tabs-6-box').val('" . $values['Conclusion'] . "');";
+		echo "});";
+	echo "</script>";
+?>
